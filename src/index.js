f2a363896683858bf6ea2b8a58160f579bfa73a2
Den Temple <developer@dentemple.com>
2018-05-02 19:15:59 -0400

    Configure Hot Module Reloading

--- src/index.js
+++ src/index.js
@@ -4,17 +4,30 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
-import App from "./App";
 
-// Uncomment for production
-// import registerServiceWorker from './util/registerServiceWorker';
+// Store a reference to the DOM element [Hot Module Reloading]
+const root = document.getElementById("root");
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+// Wrap our render in a reusable function [Hot Module Reloading]
+let render = () => {
+  const App = require("./App").default;
 
-// Uncomment for production
-// registerServiceWorker();
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    root
+  );
+};
+
+// Configures Hot Module Reloading
+if (process.env.NODE_ENV !== "production") {
+  if (module.hot) {
+    module.hot.accept("./App", () => {
+      setTimeout(render);
+    });
+  }
+}
+
+// Render the app
+render();
