2db611aa926b730fcc38604abbb5b56134e45783
Marc Aaron <marc.aaron.glasser@gmail.com>
2018-08-04 14:51:52 -0700

    Fix Broken AuthRoute

--- src/components/atoms/AuthRoute/AuthRoute.jsx
+++ src/components/atoms/AuthRoute/AuthRoute.jsx
@@ -3,24 +3,18 @@ import PropTypes from "prop-types";
 
 import { Route, Redirect } from "react-router-dom";
 
-const AuthRoute = ({
-  component: Component,
-  userInfo: { isAuthenticated },
-  ...rest
-}) => {
+const AuthRoute = ({ component: Component, user, ...rest }) => {
   return (
     <Route
       {...rest}
-      render={props =>
-        isAuthenticated ? <Component {...props} /> : <Redirect to="/" />
-      }
+      render={props => (user ? <Component {...props} /> : <Redirect to="/" />)}
     />
   );
 };
 
 AuthRoute.propTypes = {
   component: PropTypes.any.isRequired,
-  userInfo: PropTypes.object
+  user: PropTypes.object
 };
 
 export default AuthRoute;
